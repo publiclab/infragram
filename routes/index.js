5207c20159e487759aa3d9c6d905a0723d2c54b3
jywarren <jeff@unterbahn.com>
2014-01-06 11:28:38 -0500

    close Pioneer-Valley-Open-Science/infragram-js#28

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ exports.index = function(req, res){
 };
 
 exports.show = function(req, res){
-  Image.findOne({ _id: req.params.id }, 'src desc author', function (err, image) {
+  Image.findOne({ _id: req.params.id }, 'src desc author orig_src created_at log', function (err, image) {
     if (err) return handleError(err);
     res.render( 'show', {
       image : image
@@ -35,9 +35,9 @@ exports.create = function ( req, res ){
     title: req.body.title,
     author: req.body.author,
     desc: req.body.desc,
+    log: req.body.log,
     orig_src: req.body.orig_src,
     src: req.body.src,
-    log: req.body.log,
     updated_at : Date.now(),
   }).save( function( err, todo, count ){
     res.redirect( '/' );
