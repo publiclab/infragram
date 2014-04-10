c3d434c6aba2eada061639f84107fd0267d851bd
Jeffrey Warren <jeff@unterbahn.com>
2014-04-10 20:14:17 +0000

    fixing port 80

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ var ioOptions = {
 var io = require('socket.io', ioOptions).listen(server, {log: false});
 
 // all environments
-//app.set('port', process.env.PORT || 80);
-app.set('port', process.env.PORT || 8001);
+app.set('port', process.env.PORT || 80);
+//app.set('port', process.env.PORT || 8001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
