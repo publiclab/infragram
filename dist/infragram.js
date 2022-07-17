a10f37ce8bb899870c52970aa927c24cf1151802
FORCHA <forchapearl1@gmail.com>
2022-07-17 14:01:23 +0100

    Update infragram.js

--- dist/infragram.js
+++ dist/infragram.js
@@ -131,6 +131,16 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
           if (image) options.run(options.mode);
           options.camera.getSnapshot(); //if (options.colorized) return options.colorize();
         }, interval);
+      };
+
+      options.processLocalVideo = function processLocalVideo() {
+        options.camera.unInitialize();
+        var interval;
+        if (options.processor.type == "webgl") interval = 15;else interval = 150;
+        setInterval(function () {
+          if (image) options.run(options.mode);
+          options.camera.getSnapshot(); //if (options.colorized) return options.colorize();
+        }, interval);
       }; // TODO: this doesn't work; it just downloads the unmodified image. 
       // probably a timing issue?
 
@@ -399,8 +409,10 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
       function initialize() {
         getUserMedia(webRtcOptions, success, deviceError); // iOS Safari 11 compatibility: https://github.com/webrtc/adapter/issues/685
 
-        webRtcOptions.videoEl.setAttribute('autoplay', 'autoplay');
-        webRtcOptions.videoEl.setAttribute('playsinline', 'playsinline');
+        webRtcOptions.videoEl.setAttribute('id', 'webCamVideoEl');
+        var webCamVideoEl = document.getElementById('webCamVideoEl');
+        webCamVideoEl.setAttribute('autoplay', 'autoplay');
+        webCamVideoEl.setAttribute('playsinline', 'playsinline');
         window.webcam = webRtcOptions; // this is weird but maybe used for flash fallback?
 
         canvas = options.canvas || document.getElementById("image");
@@ -410,6 +422,11 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
         $("#snapshot").show();
         $("#live-video").show();
         $("#webcam").show();
+      }
+
+      function unInitialize() {
+        //Initialize Webrtc without webcam
+        getUserMedia(webRtcOptions, falseSuccess, deviceError);
       } // webRtcOptions contains the configuration information for the shim
       // it allows us to specify the width and height of the video
       // output we"re working with, the location of the fallback swf,
@@ -478,9 +495,12 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
 
       function success(stream) {
         var video;
+        window.localStream = stream;
+        isOnCam = stream;
+        isCamera = true;
 
         if (webRtcOptions.context === "webrtc") {
-          video = webRtcOptions.videoEl;
+          video = document.getElementById("webCamVideoEl");
 
           if (navigator.mozGetUserMedia) {
             video.mozSrcObject = stream;
@@ -498,6 +518,11 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
         alert("No camera available.");
         console.log(error);
         return console.error("An error occurred: [CODE " + error.code + "]");
+      }
+
+      function falseSuccess(stream) {
+        //Prevent Webcam stream during video processing
+        stream.getVideoTracks()[0].stop();
       } // not doing anything now... for copying to a 2nd canvas
 
 
@@ -524,7 +549,8 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
         getSnapshot: getSnapshot,
         initialize: initialize,
         onSaveGetUserMedia: onSaveGetUserMedia,
-        webRtcOptions: webRtcOptions
+        webRtcOptions: webRtcOptions,
+        unInitialize: unInitialize
       };
     };
   }, {}],
@@ -532,45 +558,6 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
     // This file was adapted from infragram-js:
     // http://github.com/p-v-o-s/infragram-js.
     module.exports = function File(options, processor) {
-      var _getMouseEvent = function getMouseEvent() {
-        var getParams = function getParams() {
-          return {
-            bubbles: false,
-            cancelable: false,
-            screenX: 0,
-            screenY: 0,
-            clientX: 0,
-            clientY: 0
-          };
-        };
-
-        try {
-          // eslint-disable-next-line no-new
-          new MouseEvent("t");
-
-          _getMouseEvent = function getMouseEvent() {
-            return function (eventType) {
-              var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : getParams();
-              return new MouseEvent(eventType, params);
-            };
-          };
-        } catch (e) {
-          // Polyfills DOM4 MouseEvent
-          _getMouseEvent = function getMouseEvent() {
-            return function (eventType) {
-              var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : getParams();
-              var mouseEvent = document.createEvent("MouseEvent"); // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/initMouseEvent
-
-              mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, // the event's mouse click count
-              params.screenX, params.screenY, params.clientX, params.clientY, false, false, false, false, 0, null);
-              return mouseEvent;
-            };
-          };
-        }
-
-        return _getMouseEvent();
-      };
-
       function downloadImage() {
         var event, format, lnk; // create an "off-screen" anchor tag
 
@@ -587,14 +574,9 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
         lnk.download = new Date().toISOString().replace(/:/g, "_") + "." + format; // create a "fake" click-event to trigger the download
 
         if (document.createEvent) {
-          var mouseEvent = _getMouseEvent();
-
-          lnk.dispatchEvent(mouseEvent("click", {
-            screenX: 0,
-            screenY: 0,
-            clientX: 0,
-            clientY: 0
-          }));
+          event = document.createEvent("MouseEvents");
+          event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+          lnk.dispatchEvent(event);
         } else if (lnk.fireEvent) {
           lnk.fireEvent("onclick");
         }
@@ -1457,6 +1439,9 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
   }, {}],
   15: [function (require, module, exports) {
     module.exports = function Interface(options) {
+      var isVideo = false,
+          isCamera = false;
+      var isOnCam;
       options.imageSelector = options.imageSelector || "#image-container";
       options.fileSelector = options.fileSelector || "#file-sel";
 
@@ -1528,32 +1513,107 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
           $('.choose-prompt').hide();
           $("#save-modal-btn").show();
           $("#save-zone").show();
-          var reader = new FileReader();
 
-          reader.onload = function onReaderLoad(event) {
-            var img;
-            img = new Image();
+          if (this.files[0].type == 'image/jpeg' || this.files[0].type == 'image/png') {
+            var reader = new FileReader();
 
-            img.onload = function onImageLoad() {
-              options.processor.updateImage(this);
+            reader.onload = function onReaderLoad(event) {
+              var img;
+              img = new Image();
+
+              img.onload = function onImageLoad() {
+                options.processor.updateImage(this);
+              };
+
+              return img.src = event.target.result;
             };
 
-            return img.src = event.target.result;
-          };
+            reader.readAsDataURL(this.files[0]);
+          } else {
+            videoURL = URL.createObjectURL(this.files[0]);
+            activateVideo(videoURL);
+          }
 
-          reader.readAsDataURL(this.files[0]);
-          $('#preset-modal').modal('show');
+          $('#preset-modal').offcanvas('show');
+          $('#preset-modalMobile').offcanvas('show');
           return true;
         });
         $("#webcam-activate").click(function () {
+          if (isVideo) {
+            $("video").remove(); //$("#localVideo").remove();      
+          }
+
+          isVideo = false;
+          isCamera = true;
           $('.choose-prompt').hide();
           $("#save-modal-btn").show();
           $("#save-zone").show();
           save_infragrammar_inputs();
           options.video();
-          $('#preset-modal').modal('show');
+          $('#preset-modal').offcanvas('show');
+          $('#preset-modalMobile').offcanvas('show');
           return true;
         });
+
+        function activateVideo(videoURL) {
+          options.processLocalVideo();
+
+          if (isCamera) {
+            localStream.getVideoTracks()[0].stop();
+          }
+
+          isCamera = false;
+          $("#localVideo").remove();
+          localVideo = document.createElement('video');
+          localVideo.setAttribute("id", "localVideo");
+          localVideo.setAttribute("src", videoURL);
+          localVideo.load();
+          localVideo.style.display = "none";
+          localVideo.style.width = "50px";
+          localVideo.style.height = "50px";
+          document.getElementById("video-container").appendChild(localVideo);
+          localVideo.play();
+          localVideo.muted = true;
+          localVideo.loop = true;
+          document.getElementById("localVideoControls").style.display = "block"; //Attach video Element tocustom Sleek Bar
+
+          localVideo.ontimeupdate = function () {
+            var percentage = localVideo.currentTime / localVideo.duration * 100;
+            $("#custom-seekbar span").css("width", percentage + "%");
+          };
+
+          isVideo = true;
+          $('.choose-prompt').hide();
+          $("#save-modal-btn").show();
+          $("#save-zone").show();
+          save_infragrammar_inputs();
+          $('#preset-modal').offcanvas('show');
+          $('#preset-modalMobile').offcanvas('show');
+          return true;
+        } //Start video controls
+
+
+        $("#custom-seekbar").on("click", function (e) {
+          localVideo = document.getElementById('localVideo');
+          var offset = $(this).offset();
+          var left = e.pageX - offset.left;
+          var totalWidth = $("#custom-seekbar").width();
+          var percentage = left / totalWidth;
+          var vidTime = localVideo.duration * percentage;
+          localVideo.currentTime = vidTime;
+        });
+        $("#localVideoPlayPause").on("click", function (e) {
+          localVideo = document.getElementById('localVideo');
+
+          if (localVideo.paused) {
+            localVideo.play();
+            document.getElementById("localVideoPlayPause").innerHTML = '<i class="fa fa-pause"></i>';
+          } else {
+            localVideo.pause();
+            document.getElementById("localVideoPlayPause").innerHTML = '<i class="fa fa-play"></i>';
+          }
+        }); //End video controls
+
         $("#snapshot").click(function () {
           options.camera.getSnapshot();
           return true;
@@ -1590,7 +1650,75 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
         $("[rel=tooltip]").tooltip();
         $("[rel=popover]").popover();
         return true;
+      }); //Start Handle multiple webcam resolutions
+
+      function changeResolution(w, h) {
+        document.getElementById('image').setAttribute("width", w);
+        document.getElementById('image').setAttribute("height", h);
+      }
+
+      $('#qvga').click(function (e) {
+        changeResolution('100px', '100px');
       });
+      $('#vga').click(function (e) {
+        changeResolution('800px', '600px');
+      });
+      $('#hd').click(function (e) {
+        changeResolution('2400px', '1800px');
+      });
+      $('#full-hd').click(function (e) {
+        changeResolution('6000px', '4000px');
+      }); //End Handling of Multiple webcam resolutions
+      //Start Canvas Recording and Download
+
+      var canvas = document.getElementById('image');
+      var ctx = canvas.getContext('2d');
+      var x = 0;
+      var stream = canvas.captureStream(); // grab our canvas MediaStream
+
+      var rec = new MediaRecorder(stream);
+
+      function exportVid(blob) {
+        var vid = document.createElement('video');
+        vid.src = URL.createObjectURL(blob);
+        vid.controls = true;
+        vid.style.display = 'none';
+        document.body.appendChild(vid);
+        var a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = vid.src;
+        a.download = 'infragramVideo.mp4';
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function () {
+          document.body.removeChild(a);
+          URL.revokeObjectURL(vid.src);
+        }, 100);
+      }
+
+      $('#startRecord').click(function (e) {
+        var chunks = [];
+
+        rec.ondataavailable = function (e) {
+          return chunks.push(e.data);
+        };
+
+        rec.onstop = function (e) {
+          return exportVid(new Blob(chunks, {
+            type: 'video/h264'
+          }));
+        };
+
+        rec.start();
+        document.getElementById('startRecord').style.display = 'none';
+        document.getElementById('stopRecord').style.display = 'block';
+      });
+      $('#stopRecord').click(function (e) {
+        rec.stop();
+        document.getElementById('stopRecord').style.display = 'none';
+        document.getElementById('startRecord').style.display = 'block';
+        document.getElementById('downloadButton').style.display = 'block';
+      }); //End Canvas Recording and Download
     };
   }, {
     "../color/colormaps": 5,
@@ -1608,7 +1736,8 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
         $('#r_exp').val("R");
         $('#g_exp').val("G");
         $('#b_exp').val("B");
-        $('#preset-modal').modal('hide');
+        $('#preset-modal').offcanvas('hide');
+        $('#preset-modalMobile').offcanvas('hide');
         options.colorized = false;
         options.processor.decolorize();
         save_infragrammar_inputs();
@@ -1619,7 +1748,8 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
       $("#preset_ndvi_blue").click(function () {
         $('#modeSwitcher').val("infragrammar_mono").change();
         $('#m_exp').val("(R-B)/(R+B)");
-        $('#preset-modal').modal('hide');
+        $('#preset-modal').offcanvas('hide');
+        $('#preset-modalMobile').offcanvas('hide');
         options.colorized = false;
         options.processor.decolorize();
         save_infragrammar_inputs();
@@ -1630,7 +1760,8 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
       $("#preset_ndvi_blue_color").click(function () {
         $('#modeSwitcher').val("infragrammar_mono").change();
         $('#m_exp').val("(R-B)/(R+B)");
-        $('#preset-modal').modal('hide');
+        $('#preset-modal').offcanvas('hide');
+        $('#preset-modalMobile').offcanvas('hide');
         save_infragrammar_inputs();
         options.colorized = true;
         options.run(options.mode);
@@ -1642,7 +1773,8 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
       $("#preset_ndvi_red").click(function () {
         $('#modeSwitcher').val("infragrammar_mono").change();
         $('#m_exp').val("(B-R)/(B+R)");
-        $('#preset-modal').modal('hide');
+        $('#preset-modal').offcanvas('hide');
+        $('#preset-modalMobile').offcanvas('hide');
         options.colorized = false;
         options.processor.decolorize();
         save_infragrammar_inputs();
@@ -1653,7 +1785,8 @@ function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else i
       $("#preset_ndvi_red_color").click(function () {
         $('#modeSwitcher').val("infragrammar_mono").change();
         $('#m_exp').val("(B-R)/(B+R)");
-        $('#preset-modal').modal('hide');
+        $('#preset-modal').offcanvas('hide');
+        $('#preset-modalMobile').offcanvas('hide');
         save_infragrammar_inputs();
         colorize();
         options.run(options.mode);
