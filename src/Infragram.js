0d99d51fdb57ff621f1b0fbfbc4ffc3c5f438b51
FORCHA <forchapearl1@gmail.com>
2022-07-28 07:59:13 +0100

    Update Infragram.js

--- src/Infragram.js
+++ src/Infragram.js
@@ -44,6 +44,18 @@ window.Infragram = function Infragram(options) {
     }, interval);
   }
 
+  options.processLocalVideo = function processLocalVideo() {
+    options.camera.unInitialize();
+    var interval;
+    if (options.processor.type == "webgl") interval = 15;
+    else interval = 150;
+    setInterval(function() {
+      if (image) options.run(options.mode);
+      options.camera.getSnapshot();
+      //if (options.colorized) return options.colorize();
+    }, interval);
+  }
+
   // TODO: this doesn't work; it just downloads the unmodified image. 
   // probably a timing issue?
   function download() {
