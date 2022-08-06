62f4eed91b2cde3c486a361565f1a4ccbb68709e
FORCHA <forchapearl1@gmail.com>
2022-08-06 23:23:19 +0100

    Update camera.js

--- src/io/camera.js
+++ src/io/camera.js
@@ -3,12 +3,21 @@
 module.exports = function Camera(options) {
   var canvas, 
       ctx;
+
   options.webcamVideoSelector = options.webcamVideoSelector || 'webcamVideoEl';
   options.webcamVideoEl = document.getElementById(options.webcamVideoSelector);
-  
+
   // Initialize getUserMedia with options
   function initialize() {
-    navigator.mediaDevices.getUserMedia(webRtcOptions).then(success).catch(deviceError);
+    if(options.version == 1){
+      getUserMedia(webRtcOptions, success, deviceError);
+
+      // iOS Safari 11 compatibility: https://github.com/webrtc/adapter/issues/685
+      webRtcOptions.videoEl.setAttribute('autoplay', 'autoplay');
+      webRtcOptions.videoEl.setAttribute('playsinline', 'playsinline');
+    }else{
+      navigator.mediaDevices.getUserMedia(webRtcOptions).then(success).catch(deviceError);
+    }
 
     window.webcam = webRtcOptions; // this is weird but maybe used for flash fallback?
     canvas = options.canvas || document.getElementById("image");
@@ -19,10 +28,10 @@ module.exports = function Camera(options) {
     $("#live-video").show();
     $("#webcam").show();
   }
-  
+
   function unInitialize() { //Initialize stream without webcam
     navigator.mediaDevices.getUserMedia(webRtcOptions).then(falseSuccess).catch(deviceError);
-  }  
+  }
 
   // webRtcOptions contains the configuration information for the shim
   // it allows us to specify the width and height of the video
@@ -79,15 +88,33 @@ module.exports = function Camera(options) {
   }
 
   function success(stream) {
-    window.localStream = stream;
-    isOnCam = stream;
-    isCamera = true;
-    track = stream.getTracks()[0];
-    webCamVideoEl.srcObject = stream;
+    if(options.version == 1){
 
-    return webCamVideoEl.onerror = function (e) {
-      return stream.stop();
-    };
+      var video;
+      if (webRtcOptions.context === "webrtc") {
+        video = webRtcOptions.videoEl;
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          video.srcObject = stream;
+        }
+        return video.onerror = function(e) {
+          return stream.stop();
+        }
+      } else {
+
+      }
+    }else{
+      window.localStream = stream;
+      isOnCam = stream;
+      isCamera = true;
+      track = stream.getTracks()[0];
+      webCamVideoEl.srcObject = stream;
+
+      return webCamVideoEl.onerror = function (e) {
+        return stream.stop();
+      };       
+    }
   }
 
   function deviceError(error) {
@@ -99,14 +126,35 @@ module.exports = function Camera(options) {
   function falseSuccess(stream) {
         //Stream for video processing
         stream.getVideoTracks()[0].stop();
-  }  
-  
+  }
+
   // not doing anything now... for copying to a 2nd canvas
   function getSnapshot() {
-    var video;
+    if(options.version == 1){
+
+      var video;
+      // If the current context is WebRTC/getUserMedia (something
+      // passed back from the shim to avoid doing further feature
+      // detection), we handle getting video/images for our canvas 
+      // from our HTML5 <video> element.
+      if (webRtcOptions.context === "webrtc") {
+        video = document.getElementsByTagName("video")[0];
+        options.processor.updateImage(video);
+        return $("#webcam").hide();
+      // Otherwise, if the context is Flash, we ask the shim to
+      // directly call window.webcam, where our shim is located
+      // and ask it to capture for us.
+      } else if (webRtcOptions.context === "flash") {
+        return window.webcam.capture();
+      } else {
+        console.log("No context was supplied to getSnapshot()");
+      }
+    }else{
+      var video;
       video = document.getElementsByTagName("video")[0];
       options.processor.updateImage(video);
       return $("#webcam").hide();
+    }
   }
 
   return {
@@ -115,5 +163,6 @@ module.exports = function Camera(options) {
     onSaveGetUserMedia: onSaveGetUserMedia,
     webRtcOptions: webRtcOptions,
     unInitialize: unInitialize,
+
   }
 }
